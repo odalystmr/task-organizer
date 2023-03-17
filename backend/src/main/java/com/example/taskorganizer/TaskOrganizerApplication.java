c07f367124b2fe83e766fdc7ac79658886bfb0a4
odalystmr <odalystmr@gmail.com>
2023-03-17 10:06:52 +0100

    first commit - Spring and Angular installed

---
+++ backend/src/main/java/com/example/taskorganizer/TaskOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskOrganizerApplication.class, args);
+    }
+
+}
