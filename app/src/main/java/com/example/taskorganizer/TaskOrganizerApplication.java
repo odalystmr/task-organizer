8c071e8b339a954b86f9fc88f7fb1e3a16c57ac5
odalystmr <odalystmr@gmail.com>
2025-06-29 15:05:30 +0200

    fix cors

--- app/src/main/java/com/example/taskorganizer/TaskOrganizerApplication.java
+++ app/src/main/java/com/example/taskorganizer/TaskOrganizerApplication.java
@@ -2,6 +2,9 @@ package com.example.taskorganizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TaskOrganizerApplication {
@@ -10,4 +13,16 @@ public class TaskOrganizerApplication {
         SpringApplication.run(TaskOrganizerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://organizer.odalystnr.com")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
